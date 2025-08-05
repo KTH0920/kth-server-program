b8865e47937926b36a78dfdfc78aefa8afe087d3
KTH0920 <taehyun9482@gmail.com>
2025-08-05 11:46:00 +0900

    ,

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const userRouter = require("./routes/user")
+const booksRoutes = require('./routes/books')
+
 app.use(express.json());
-app.use("/users",userRouter)
+app.use("/books", booksRoutes)
+
 
 app.get("/", (req, res) => {
   res.send("Hellod World");
